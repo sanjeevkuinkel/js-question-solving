f6712f6f43d647e56fcc0cc73b22e4a6b30c6a59
sanjeevkuinkel <sanjeeevkuikel1111@gmail.com>
2026-01-28 20:49:25 +0545

    question3 understand and solved

--- question3.js
+++ question3.js
@@ -1,3 +1,16 @@
 //!Write a function to find the second largest number in an array.
+let arr = [10, 5, 8, 20, 15];
 
-let numberArray=[1,2,4,0,6,9,3];
+let largest = -Infinity;
+let secondLargest = -Infinity;
+
+for (let num of arr) {
+  if (num > largest) {
+    secondLargest = largest;
+    largest = num;
+  } else if (num > secondLargest && num !== largest) {
+    secondLargest = num;
+  }
+}
+
+console.log(secondLargest); // 15
